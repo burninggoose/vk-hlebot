bf8426e3e2d4feb0150dab321a08268b687d1050
Gleb Andreev <gleb.andreev2000@gmail.com>
2018-11-17 19:45:52 +0300

    Initial commit

---
+++ index.py
@@ -0,0 +1,27 @@
+import vk_api
+import random
+import os
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def main():
+    vk_session = vk_api.VkApi(
+        login=None, password=None, token=os.environ['TOKEN'])
+    vk = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+
+   # vk.messages.send(
+    #    chat_id=10, message='Бот хлеба: Выпускаем кракена')
+
+    for event in longpoll.listen():
+        if hasattr(event, 'type'):
+            if event.type == VkEventType.MESSAGE_NEW:
+                if event.from_chat:
+                    if (event.chat_id == 10):
+                        if (event.text == '!флип'):
+                            vk.messages.send(
+                                chat_id=10, message='Хлебот: ' + random.choice(['Орел', 'Решка']))
+
+
+if __name__ == '__main__':
+    main()
